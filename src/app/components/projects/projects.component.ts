6cd00edaff7a5573a49409e98748f67b82155e8b
YARE0909 <pradyumnad092004@gmail.com>
2023-06-19 16:46:01 +0530

    💄 Added projects page

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      project: "FACIAL VERIFICATION APP",
+      link: "https://capx-facial-recognition.vercel.app/"
+    },
+    {
+      project: "INFOSTASH WEBSITE",
+      link: "https://infostash-web.vercel.app/"
+    },
+    {
+      project: "MOZOHACK 4.0",
+      link: "https://mozohack.srmkzilla.net/"
+    },
+    {
+      project: "MOZOFEST 2023",
+      link: "https://mozofest.srmkzilla.net/"
+    },
+  ]
 }
